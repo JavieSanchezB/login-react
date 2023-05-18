e2d3ae19f4281f13563252e89c9a40455759d031
horusjavier <kornjaviersanchezb@gmail.com>
2023-05-18 16:39:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./contexts/user.context";
+import Home from "./pages/Home.page";
+import Login from "./pages/Login.page";
+import PrivateRoute from "./pages/PrivateRoute.page";
+import Signup from "./pages/Signup.page";
+ 
+function App() {
+ return (
+   <BrowserRouter>
+     {/* We are wrapping our whole app with UserProvider so that */}
+     {/* our user is accessible through out the app from any page*/}
+     <UserProvider>
+       <Routes>
+         <Route exact path="/login" element={<Login />} />
+         <Route exact path="/signup" element={<Signup />} />
+         {/* We are protecting our Home Page from unauthenticated */}
+         {/* users by wrapping it with PrivateRoute here. */}
+         <Route element={<PrivateRoute />}>
+           <Route exact path="/" element={<Home />} />
+         </Route>
+       </Routes>
+     </UserProvider>
+   </BrowserRouter>
+ );
+}
+ 
+export default App;
---
+++ src/realm/constants.js
@@ -0,0 +1,2 @@
+
+export const APP_ID = "application-0-ebfvv";
