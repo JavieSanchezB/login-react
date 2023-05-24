bf48ac21b41ca48dde618998dd471691a2a49322
Javier Sanchez B <ing.javier.sanchez.b@gmail.com>
2023-05-23 22:02:49 -0500

    vista home

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -13,28 +13,7 @@ const client = new MongoClient(url);
 
 // Database Name
 const dbName = process.env.DBNAME;
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('usuarios');
 
-
-const insertResult = await collection.insertMany([{ id: 1, nombres:"Jose" ,apellidos:"Mejia Bolaños", Rol:"Contador" }, { id: 2, nombres:"Fabian" ,apellidos:"Jimenez", Rol:"Administrador" }, { id: 3, nombres:"Fabian" ,apellidos:"Jimenez", Rol:"Caja"}]);
-console.log('Inserted documents =>', insertResult);
-
-  // the following code examples can be pasted here...
-  const findResult = await collection.find({}).toArray();
-  console.log('Found documents =>', findResult);
-
-  return 'done.';
-}
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
  
 export default function Home() {
  const { logOutUser } = useContext(UserContext);
@@ -60,7 +39,7 @@ export default function Home() {
      <h1>Bienvenido,Primera vista</h1>
      
      <Button variant="contained" onClick={logOut}>Cerrar Sesión</Button>
-     <Button variant="contained" onClick={main}>Consultar</Button>
+     <Button variant="contained" onClick={}>Consultar</Button>
    </>
  )
 }
