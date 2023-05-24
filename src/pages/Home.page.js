2e167ed1f9292117b55f47a1f30b5f5dbab7f1f1
Javier Sanchez B <ing.javier.sanchez.b@gmail.com>
2023-05-23 21:55:46 -0500

    actualizada vista home

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -13,25 +13,7 @@ const client = new MongoClient(url);
 
 // Database Name
 const dbName = process.env.DBNAME;
- 
-export default function Home() {
- const { logOutUser } = useContext(UserContext);
- 
- // This function is called when the user clicks the "Logout" button.
- const logOut = async () => {
-   try {
-     // Calling the logOutUser function from the user context.
-     const loggedOut = await logOutUser();
-     // Now we will refresh the page, and the user will be logged out and
-     // redirected to the login page because of the <PrivateRoute /> component.
-     if (loggedOut) {
-       window.location.reload(true);
-     }
-   } catch (error) {
-     alert(error)
-   }
- }
- async function main() {
+async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
@@ -53,6 +35,25 @@ main()
   .then(console.log)
   .catch(console.error)
   .finally(() => client.close());
+ 
+export default function Home() {
+ const { logOutUser } = useContext(UserContext);
+ 
+ // This function is called when the user clicks the "Logout" button.
+ const logOut = async () => {
+   try {
+     // Calling the logOutUser function from the user context.
+     const loggedOut = await logOutUser();
+     // Now we will refresh the page, and the user will be logged out and
+     // redirected to the login page because of the <PrivateRoute /> component.
+     if (loggedOut) {
+       window.location.reload(true);
+     }
+   } catch (error) {
+     alert(error)
+   }
+ }
+ 
  
  return (
    <>
