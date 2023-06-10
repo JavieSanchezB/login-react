636b7912f76f912b572ec78b43ad316605cc94c3
Javier Sanchez B <ing.javier.sanchez.b@gmail.com>
2023-06-09 22:22:19 -0500

    update v4

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,146 +1,127 @@
-import React from 'react';
-import { CSSTransition } from 'react-transition-group';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { CSSTransition } from "react-transition-group";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Esquema de validación con Yup
+const validationSchema = Yup.object().shape({
+  codigo: Yup.string().required("Campo requerido"),
+  fechaMatricula: Yup.date().required("Campo requerido"),
+  primerNombre: Yup.string().required("Campo requerido"),
+  segundoNombre: Yup.string(),
+  primerApellido: Yup.string().required("Campo requerido"),
+  segundoApellido: Yup.string(),
+  fechaNacimiento: Yup.date().required("Campo requerido"),
+  municipioNacimiento: Yup.string().required("Campo requerido"),
+  departamentoNacimiento: Yup.string().required("Campo requerido"),
+  identificacion: Yup.string().required("Campo requerido"),
+  sexo: Yup.string().required("Campo requerido"),
+  departamentoExpedicion: Yup.string().required("Campo requerido"),
+  municipioExpedicion: Yup.string().required("Campo requerido"),
+  zonaResidencia: Yup.string().required("Campo requerido"),
+  estrato: Yup.string().required("Campo requerido"),
+  sisben: Yup.string().required("Campo requerido"),
+  direccionResidencia: Yup.string().required("Campo requerido"),
+  telefono: Yup.string().required("Campo requerido"),
+  barrio: Yup.string().required("Campo requerido"),
+  municipio: Yup.string().required("Campo requerido"),
+  gradoSolicitado: Yup.string().required("Campo requerido"),
+  grupo: Yup.string().required("Campo requerido"),
+  jornada: Yup.string().required("Campo requerido"),
+  repitente: Yup.string().required("Campo requerido"),
+  continuidadGrupo: Yup.string().required("Campo requerido"),
+  continuidadAnio: Yup.string().required("Campo requerido"),
+  alumnoProvieneSectorPrivado: Yup.string().required("Campo requerido"),
+  provieneOtroMunicipio: Yup.string().required("Campo requerido"),
+  alumnoVictimaConflictos: Yup.string().required("Campo requerido"),
+  codigoDepartamentoExpulsor: Yup.string().required("Campo requerido"),
+  codigoMunicipioExpulsor: Yup.string().required("Campo requerido"),
+  alumnoDiscapacidad: Yup.string().required("Campo requerido"),
+  alumnoPerteneceEtnia: Yup.string().required("Campo requerido"),
+  institucionFamiliar: Yup.string().required("Campo requerido"),
+  nombreAcudiente: Yup.string().required("Campo requerido"),
+  apellidosAcudiente: Yup.string().required("Campo requerido"),
+  identificacionAcudiente: Yup.string().required("Campo requerido"),
+  telefonoAcudiente: Yup.string().required("Campo requerido"),
+  direccionAcudiente: Yup.string().required("Campo requerido"),
+  observaciones: Yup.string().required("Campo requerido"),
+});
 
 const Home = () => {
-  const initialValues = {
-    codigo: '',
-    fechaMatricula: '',
-    primerNombre: '',
-    segundoNombre: '',
-    primerApellido: '',
-    segundoApellido: '',
-    fechaNacimientoDia: '',
-    fechaNacimientoMes: '',
-    fechaNacimientoAnio: '',
-    municipioNacimiento: '',
-    departamentoNacimiento: '',
-    identificacionTipo: '',
-    identificacionNumero: '',
-    sexo: '',
-    departamentoExpedicion: '',
-    municipioExpedicion: '',
-    zonaResidencia: '',
-    estrato: '',
-    sisben: '',
-    direccionResidencia: '',
-    telefono: '',
-    barrio: '',
-    municipio: '',
-    gradoSolicitado: '',
-    grupo: '',
-    repitente: '',
-    continuidadGrupo: '',
-    continuidadAnio: '',
-    jornada: '',
-    alumnoProvieneSectorPrivado: '',
-    provieneOtroMunicipio: '',
-    alumnoVictimaConflictoArmado: '',
-    codigoDepartamentoExpulsor: '',
-    codigoMunicipioExpulsor: '',
-    alumnoTieneDiscapacidad: '',
-    alumnoPerteneceEtnia: '',
-    institucionFamiliar: '',
-    acudienteNombre: '',
-    acudienteApellidos: '',
-    acudienteIdentificacionTipo: '',
-    acudienteIdentificacionNumero: '',
-    acudienteTelefono: '',
-    acudienteDireccion: '',
-    observaciones: '',
-  };
-
-  const validationSchema = Yup.object().shape({
-    codigo: Yup.string().required('Este campo es obligatorio'),
-    fechaMatricula: Yup.date().required('Este campo es obligatorio'),
-    primerNombre: Yup.string().required('Este campo es obligatorio'),
-    segundoNombre: Yup.string(),
-    primerApellido: Yup.string().required('Este campo es obligatorio'),
-    segundoApellido: Yup.string(),
-    fechaNacimientoDia: Yup.number().required('Este campo es obligatorio'),
-    fechaNacimientoMes: Yup.number().required('Este campo es obligatorio'),
-    fechaNacimientoAnio: Yup.number().required('Este campo es obligatorio'),
-    municipioNacimiento: Yup.string().required('Este campo es obligatorio'),
-    departamentoNacimiento: Yup.string().required('Este campo es obligatorio'),
-    identificacionTipo: Yup.string().required('Este campo es obligatorio'),
-    identificacionNumero: Yup.string().required('Este campo es obligatorio'),
-    sexo: Yup.string().required('Este campo es obligatorio'),
-    departamentoExpedicion: Yup.string().required('Este campo es obligatorio'),
-    municipioExpedicion: Yup.string().required('Este campo es obligatorio'),
-    zonaResidencia: Yup.string().required('Este campo es obligatorio'),
-    estrato: Yup.string().required('Este campo es obligatorio'),
-    sisben: Yup.string().required('Este campo es obligatorio'),
-    direccionResidencia: Yup.string().required('Este campo es obligatorio'),
-    telefono: Yup.string().required('Este campo es obligatorio'),
-    barrio: Yup.string().required('Este campo es obligatorio'),
-    municipio: Yup.string().required('Este campo es obligatorio'),
-    gradoSolicitado: Yup.string().required('Este campo es obligatorio'),
-    grupo: Yup.string().required('Este campo es obligatorio'),
-    repitente: Yup.string().required('Este campo es obligatorio'),
-    continuidadGrupo: Yup.string(),
-    continuidadAnio: Yup.string(),
-    jornada: Yup.string().required('Este campo es obligatorio'),
-    alumnoProvieneSectorPrivado: Yup.string().required('Este campo es obligatorio'),
-    provieneOtroMunicipio: Yup.string().required('Este campo es obligatorio'),
-    alumnoVictimaConflictoArmado: Yup.string().required('Este campo es obligatorio'),
-    codigoDepartamentoExpulsor: Yup.string(),
-    codigoMunicipioExpulsor: Yup.string(),
-    alumnoTieneDiscapacidad: Yup.string().required('Este campo es obligatorio'),
-    alumnoPerteneceEtnia: Yup.string().required('Este campo es obligatorio'),
-    institucionFamiliar: Yup.string(),
-    acudienteNombre: Yup.string().required('Este campo es obligatorio'),
-    acudienteApellidos: Yup.string().required('Este campo es obligatorio'),
-    acudienteIdentificacionTipo: Yup.string().required('Este campo es obligatorio'),
-    acudienteIdentificacionNumero: Yup.string().required('Este campo es obligatorio'),
-    acudienteTelefono: Yup.string().required('Este campo es obligatorio'),
-    acudienteDireccion: Yup.string().required('Este campo es obligatorio'),
-    observaciones: Yup.string(),
-  });
-
   const handleSubmit = (values) => {
     console.log(values);
-    toast.success('Formulario enviado exitosamente');
+    toast.success("Formulario enviado correctamente");
   };
 
   return (
     <div>
-      <ToastContainer />
       <button
         type="button"
         className="btn btn-primary"
-        onClick={() => toast.info('Mostrar Formulario')}
+        onClick={() => toast.info("Mostrando formulario")}
       >
         Mostrar Formulario
       </button>
-      <CSSTransition in={true} appear={true} timeout={500} classNames="fade">
+
+      <CSSTransition
+        in={true}
+        appear={true}
+        timeout={500}
+        classNames="fade"
+        unmountOnExit
+      >
         <Formik
-          initialValues={initialValues}
+          initialValues={{
+            codigo: "",
+            fechaMatricula: "",
+            primerNombre: "",
+            segundoNombre: "",
+            primerApellido: "",
+            segundoApellido: "",
+            fechaNacimiento: "",
+            municipioNacimiento: "",
+            departamentoNacimiento: "",
+            identificacion: "",
+            sexo: "",
+            departamentoExpedicion: "",
+            municipioExpedicion: "",
+            zonaResidencia: "",
+            estrato: "",
+            sisben: "",
+            direccionResidencia: "",
+            telefono: "",
+            barrio: "",
+            municipio: "",
+            gradoSolicitado: "",
+            grupo: "",
+            jornada: "",
+            repitente: "",
+            continuidadGrupo: "",
+            continuidadAnio: "",
+            alumnoProvieneSectorPrivado: "",
+            provieneOtroMunicipio: "",
+            alumnoVictimaConflictos: "",
+            codigoDepartamentoExpulsor: "",
+            codigoMunicipioExpulsor: "",
+            alumnoDiscapacidad: "",
+            alumnoPerteneceEtnia: "",
+            institucionFamiliar: "",
+            nombreAcudiente: "",
+            apellidosAcudiente: "",
+            identificacionAcudiente: "",
+            telefonoAcudiente: "",
+            direccionAcudiente: "",
+            observaciones: "",
+          }}
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
         >
-          {({ errors, touched }) => (
+          {({ handleChange, values, errors }) => (
             <Form>
-              <h3>Corporación Politécnico de Magangué</h3>
-              <h4>BACHILLERATO 2023</h4>
-
-              <div className="form-group">
-                <label>CÓDIGO:</label>
-                <Field type="text" name="codigo" className="form-control" />
-                <ErrorMessage name="codigo" component="div" className="text-danger" />
-              </div>
-
-              <div className="form-group">
-                <label>FECHA DE MATRICULA:</label>
-                <Field type="date" name="fechaMatricula" className="form-control" />
-                <ErrorMessage name="fechaMatricula" component="div" className="text-danger" />
-              </div>
-
-              {/* Resto de los campos del formulario */}
-
+              <h3>TARJETA ACUMULATIVA DE MATRICULA</h3>
+              {/* Resto del formulario */}
               <button type="submit" className="btn btn-primary">
                 Enviar
               </button>
@@ -148,6 +129,8 @@ const Home = () => {
           )}
         </Formik>
       </CSSTransition>
+
+      <ToastContainer />
     </div>
   );
 };
